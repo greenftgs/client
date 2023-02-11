244795fce8313c77de0c08a343b502011b451e10
greenftgs <greenftgs@gmail.com>
2023-02-11 15:21:06 +0300

    перед отправкой

--- search pub.py
+++ search pub.py
@@ -23,6 +23,6 @@ def get_shops(publisher):
 
 if __name__ == '__main__':
     search_pub = input('Введите имя или id издателя для поиска книг в магазине: ').lower()
-    print(get_shops(publisher))
+    print(get_shops(search_pub))
 
 db.close()
